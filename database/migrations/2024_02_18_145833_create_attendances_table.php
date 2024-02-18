011f3e60d1b53459f19b13afbaf087a5b21987e4
haith <haithachxa2002@gmail.com>
2024-02-18 22:20:32 +0700

    update migration

---
+++ database/migrations/2024_02_18_145833_create_attendances_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('club_session_id');
+            $table->uuid('student_id');
+            $table->boolean('present');
+            $table->timestamps();
+
+            $table->foreign('club_session_id')->references('id')->on('club_sessions');
+            $table->foreign('student_id')->references('id')->on('students');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
